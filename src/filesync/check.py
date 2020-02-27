5397fd80b18270d025bdd0c90299861e83cb9e96
BubblesToTheLimit <felix.brilej@googlemail.com>
2020-02-27 17:41:46 +0100

    Fix generation of lists

--- src/filesync/check.py
+++ src/filesync/check.py
@@ -1,16 +1,30 @@
-from os import path, scandir
+import os
 import sys
 
-def generate_source_list(path_or_file):
+
+def generate_source_list(dir_or_file):
     """
     Generate a list of files from a given file or path
     """
-    if path.isfile(path_or_file):
-        return [path_or_file]
-    elif path.isdir(path_or_file):
-        with scandir(path_or_file) as it:
-            return [x for x in it if path.isfile(x)]
+    if os.path.isfile(dir_or_file):
+        return [dir_or_file]
+    elif os.path.isdir(dir_or_file):
+        with os.scandir(dir_or_file) as it:
+            return [x for x in it if os.path.isfile(x)]
     else:
-        print(f"Source Path {path_or_file} is invalid!")
+        print(f"Source Path {dir_or_file} is invalid!")
         sys.exit(1)
 
+def generate_destination_list(given_path):
+    """
+    Generate a list of files from a given path, including sub-paths.
+    """
+    result = []
+
+    if not os.path.isdir(given_path):
+        print(f"Destination Path {given_path} is invalid!")
+        sys.exit(1)
+    else:
+        for root, dirs, files in os.walk(given_path):
+            result += [os.path.relpath(os.path.join(root, file)) for file in files]
+        return result
--- tests/test_check.py
+++ tests/test_check.py
@@ -1,7 +1,8 @@
 import pytest
 
-from os import path
 from filesync import check
+
+from os import walk
 
 source_example = [
     "fileA",
@@ -10,7 +11,7 @@ source_example = [
 ]
 
 destination_example = [
-    "fileA"
+    "fileA",
     "folder1/fileB"
 ]
 
@@ -20,7 +21,7 @@ def test_source_list_generation_from_file(mocker):
     """
     single_file = source_example[0]
     
-    mocker.patch("os.path.isfile", return_values=[True])
+    mocker.patch("os.path.isfile", return_value=True)
     assert check.generate_source_list(single_file) == [single_file]
 
 
@@ -28,11 +29,12 @@ def test_destination_list_generation(mocker):
     """
     Given a destination containing directories, the function should correctly return a list.
     """
-    test_directory = "somedir"
+    test_directory = "."
 
-    mocker.patch("os.fwalk", return_values=[
-        (test_directory, ["folder1"], ["fileA"], 1),
-        (test_directory + "/folder1", [], ["fileB"], 1)
-    ])
+    mocker.patch("os.walk", return_value=iter([
+        (test_directory, ["folder1"], ["fileA"]),
+        (test_directory + "/folder1", [], ["fileB"])
+    ]))
+    mocker.patch("os.path.isdir", return_value=True)
 
     assert check.generate_destination_list(test_directory) == destination_example
